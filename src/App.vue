acacbced08d5fce1704d7fb679c2429dcc4871c1
Matthew Dawkins <matthew.dawkins@gmail.com>
2025-11-09 13:31:40 +0000

    Add tab switcher

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,40 @@
 <script setup>
 import Frontington from "./components/Frontington.vue";
+import { onMounted, ref } from "vue";
+
+const activeTab = ref(2);
+
+const selectPrevious = () => {
+  activeTab.value--;
+  if (activeTab.value < 0) {
+    activeTab.value = 2;
+  }
+};
+const selectNext = () => {
+  activeTab.value++;
+  if (activeTab.value > 2) {
+    activeTab.value = 0;
+  }
+};
 </script>
 
 <template>
-  <div class="signal-box">
-    <Frontington />
+  <div
+    class="signal-box"
+    @keydown.left="selectPrevious"
+    @keydown.right="selectNext"
+  >
+    <div class="switcher">
+      <button
+        v-for="(tab, index) in ['Tutherside', 'Yard', 'Frontington']"
+        :key="index"
+        :class="{ active: activeTab === index }"
+        @click="activeTab = index"
+      >
+        {{ tab }}
+      </button>
+    </div>
+    <Frontington v-show="activeTab === 2" />
     <footer>
       <div>
         <span
@@ -31,6 +61,21 @@ import Frontington from "./components/Frontington.vue";
 body {
   font-family: "Jost", sans-serif;
   line-height: 1;
+}
+
+.switcher {
+  padding: 1rem 3rem;
+  display: flex;
+  justify-content: center;
+  gap: 1rem;
+}
+.switcher button {
+  font: inherit;
+  font-size: 2rem;
+  padding: 0.5rem 3rem;
+  &.active {
+    background: gray;
+  }
 }
 
 footer {
